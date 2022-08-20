6f60424f0f9ff05206d5be6c4f2a0d5b989ca6a6
LABCAT <shane.watters.nz@gmail.com>
2022-08-20 17:40:33 +1200

    updating sketch to use HSB with random hues and only show up to four sparkles at a time.

--- src/js/P5SketchWithAudio.js
+++ src/js/P5SketchWithAudio.js
@@ -59,28 +59,45 @@ const P5SketchWithAudio = () => {
             }
         } 
 
-        p.numOfLoops = 1000;
+        p.numOfLoops = 2000;
+
         p.vx = new Array(p.numOfLoops);
         p.vy = new Array(p.numOfLoops);
         p.x = new Array(p.numOfLoops);
         p.y = new Array(p.numOfLoops);
         p.ax = new Array(p.numOfLoops);
         p.ay = new Array(p.numOfLoops);
+        
+        p.vx2 = new Array(p.numOfLoops);
+        p.vy2 = new Array(p.numOfLoops);
+        p.x2 = new Array(p.numOfLoops);
+        p.y2 = new Array(p.numOfLoops);
+        p.ax2 = new Array(p.numOfLoops);
+        p.ay2 = new Array(p.numOfLoops);
+
         p.touchX = 0;
         p.touchY = 0;
+        p.hueOptions = [0, 30, 60, 80, 120, 150, 180, 210, 240, 270, 300, 330];
+        p.hueRangeMin = p.random(p.hueOptions);
+        p.hueRangeMax = p.hueRangeMin + 30;
 
-        p.magnetism = 20.0;
+        p.magnetism = 444.0;
+        
         p.radius = 1;
         p.gensoku = 0.95; // principle
 
         p.setup = () => {
             p.canvas = p.createCanvas(p.canvasWidth, p.canvasHeight);
             p.noStroke(); 
+            p.background(0);
             p.fill(0);
             p.ellipseMode(p.RADIUS);
-            p.background(0);
+            p.colorMode(p.HSB);
             p.blendMode(p.ADD);
-            
+            p.setupParticles();
+        }
+
+        p.setupParticles = () => {
             for(var i =0; i< p.numOfLoops; i++){
                 p.x[i] = p.random(p.width);
                 p.y[i] = p.random(p.height);
@@ -92,11 +109,8 @@ const P5SketchWithAudio = () => {
         }
 
         p.draw = () => {
-            
             if(p.audioLoaded && p.song.isPlaying()){
                 p.background(0);
-                p.fill(0,0,0);
-                p.rect(0,0,p.width,p.height);
                 for(let i=0; i < p.numOfLoops; i++){
                     const distance = p.dist(p.touchX, p.touchY, p.x[i], p.y[i]);
 
@@ -114,21 +128,48 @@ const P5SketchWithAudio = () => {
                     p.y[i] += p.vy[i]; 
                     
                     const sokudo = p.dist(0, 0, p.vx[i], p.vy[i]);
-                    const r = p.map(sokudo, 0, 5, 0, 255); 
-                    const g = p.map(sokudo, 0, 5, 64, 255);
-                    // const b = p.map(sokudo, 0, 5, 0, 128);
-                    // const r = p.random(0, 255);
-                    // const g = p.random(128, 255);
-                    const b = p.random(0, 128);
-                    p.fill(r, g, b, 64);
+                    const r = p.map(sokudo, 0, 5, p.hueRangeMin, p.hueRangeMax); 
+                    const g = p.map(sokudo, 0, 5, 75, 100);
+                    const b = p.map(sokudo, 0, 5, 75, 100);
+                    p.fill(r, g, b, 0.1);
                     p.ellipse(p.x[i], p.y[i], p.radius, p.radius);
                 }
             }
         }
 
         p.executeCueSet1 = (note) => {
+            const { currentCue } = note;
+            if(currentCue % 4 === 1) {
+                p.clear();
+            }
             p.touchX = p.random(0, p.width);
             p.touchY = p.random(0, p.height);
+            p.hueRangeMin = p.random(p.hueOptions);
+            p.hueRangeMax = p.hueRangeMin + 30;
+            p.magnetism = p.random(88, 444);
+        }
+
+        p.coOrds = [];
+
+        p.generateCoOrds = () => {
+            p.coOrds = [];
+            for (let i = 0; i < 2; i++) {
+                p.coOrds.push(
+                    {
+                        x: p.random((p.width / 2) * i, (p.width / 2) * i + (p.width / 2)),
+                        y: p.random(0, (p.height / 2))
+                    }
+                )
+            }
+            for (let i = 0; i < 2; i++) {
+                p.coOrds.push(
+                    {
+                        x: p.random((p.width / 2) * i, (p.width / 2) * i + (p.width / 2)),
+                        y: p.random((p.height / 2), p.height)
+                    }
+                )
+            }
+            p.coOrds = p.shuffle(p.coOrds);
         }
 
         p.hasStarted = false;
